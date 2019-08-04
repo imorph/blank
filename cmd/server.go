9b2a67eda01598a911ec60c8e0a97c055c3bfc7d
Ivan Goncharov <i.morph@gmail.com>
2019-08-05 00:28:01 +0300

    FIX: get address

--- cmd/server.go
+++ cmd/server.go
@@ -41,9 +41,9 @@ var serverCmd = &cobra.Command{
 		fmt.Println("server called")
 
 		startTime := time.Now()
-		go health.ListenAndServe(address)
+		go health.ListenAndServe(viper.GetString("address"))
 		log.Println("Started App in:", time.Since(startTime))
-		log.Println("Listen on:", address)
+		log.Println("Listen on:", viper.GetString("address"))
 
 		signal := signl.WaitForSigterm()
 		log.Println("recieved signal:", signal)
@@ -62,11 +62,10 @@ func init() {
 	// and all subcommands, e.g.:
 	serverCmd.PersistentFlags().StringVar(&address, "address", "127.0.0.1:9999", "An adress server will listen on")
 	err := viper.BindPFlag("address", serverCmd.PersistentFlags().Lookup("address"))
-	viper.SetDefault("address", "127.0.0.1:9999")
-
 	if err != nil {
 		log.Println("unable to bing to flag", err)
 	}
+	viper.SetDefault("address", "127.0.0.1:9999")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
