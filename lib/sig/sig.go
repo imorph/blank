ee45381c9cb2f14928d80a1a27058fdf5c24cf94
Ivan Goncharov <i.morph@gmail.com>
2019-08-04 20:51:36 +0300

    first commit

---
+++ lib/sig/sig.go
@@ -0,0 +1,15 @@
+package sig
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// WaitForSigterm waits for either SIGTERM or SIGINT
+// Returns the caught signal.
+func WaitForSigterm() os.Signal {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGTERM)
+	return <-ch
+}
